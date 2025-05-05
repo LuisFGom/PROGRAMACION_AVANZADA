3e8f035912bc66aa0e2c3b8b5aa610ef496c35cf
LuisFGom <luisgomezllerena2.0@gmail.com>
2025-05-05 09:59:46 -0500

    Add files via upload

---
+++ Metodos y Listas/Metodos y Listas/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Metodos_y_Listas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtElemento = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lstElementos = new System.Windows.Forms.ListBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtElemento
+            // 
+            this.txtElemento.Location = new System.Drawing.Point(301, 57);
+            this.txtElemento.Name = "txtElemento";
+            this.txtElemento.Size = new System.Drawing.Size(324, 22);
+            this.txtElemento.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(93, 146);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(100, 41);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lstElementos
+            // 
+            this.lstElementos.FormattingEnabled = true;
+            this.lstElementos.ItemHeight = 16;
+            this.lstElementos.Location = new System.Drawing.Point(301, 137);
+            this.lstElementos.Name = "lstElementos";
+            this.lstElementos.Size = new System.Drawing.Size(324, 228);
+            this.lstElementos.TabIndex = 2;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(93, 207);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(100, 40);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ingrese el Dato que desea añadir a la Lista:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.lstElementos);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtElemento);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtElemento;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListBox lstElementos;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
